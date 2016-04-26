fc53a9fdfdbd9558a0bef14fd229bc0fa359220f
hverlin <hugues.verlin@gmail.com>
2016-04-26 10:58:25 +0200

    ajout du nouveau style (2)

--- src/client/app/components/navbar.component.ts
+++ src/client/app/components/navbar.component.ts
@@ -1,10 +1,15 @@
 import {Component} from 'angular2/core';
 import {ROUTER_DIRECTIVES} from 'angular2/router';
+import {ACCORDION_DIRECTIVES} from 'ng2-bootstrap';
+import { CORE_DIRECTIVES, FORM_DIRECTIVES } from 'angular2/common';
 
 @Component({
   selector: 'sd-navbar',
   templateUrl: 'app/components/navbar.component.html',
   styleUrls: ['app/components/navbar.component.css'],
-  directives: [ROUTER_DIRECTIVES]
+  directives: [ACCORDION_DIRECTIVES, CORE_DIRECTIVES, FORM_DIRECTIVES, ROUTER_DIRECTIVES]
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+    open:boolean = false
+    
+}
