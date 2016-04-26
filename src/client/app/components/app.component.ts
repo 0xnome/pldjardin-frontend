9d104c3922ab5ae4932a23a5720c2e720c40583c
hverlin <hugues.verlin@gmail.com>
2016-04-26 08:22:16 +0200

    fix toolbar component

--- src/client/app/components/app.component.ts
+++ src/client/app/components/app.component.ts
@@ -1,7 +1,6 @@
 import {Component} from 'angular2/core';
 import {ROUTER_DIRECTIVES, RouteConfig} from 'angular2/router';
 import {NavbarComponent} from './navbar.component';
-import {ToolbarComponent} from './toolbar.component';
 import {NameListService} from '../shared/index';
 import {HomeComponent} from '../+home/index';
 import {AboutComponent} from '../+about/index';
@@ -10,7 +9,7 @@ import {AboutComponent} from '../+about/index';
   selector: 'sd-app',
   viewProviders: [NameListService],
   templateUrl: 'app/components/app.component.html',
-  directives: [ROUTER_DIRECTIVES, NavbarComponent, ToolbarComponent]
+  directives: [ROUTER_DIRECTIVES, NavbarComponent]
 })
 @RouteConfig([
   {
