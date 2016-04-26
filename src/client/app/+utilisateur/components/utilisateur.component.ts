16ebfe95dce084d0df18cb495a9711aec4f054c9
hverlin <hugues.verlin@gmail.com>
2016-04-26 16:02:27 +0200

    ajout utilisateur

---
+++ src/client/app/+utilisateur/components/utilisateur.component.ts
@@ -0,0 +1,22 @@
+import {Component} from 'angular2/core';
+import {FORM_DIRECTIVES, CORE_DIRECTIVES} from "angular2/common";
+import {RouteParams, Router} from 'angular2/router';
+
+@Component({
+    selector: 'sd-jardin',
+    templateUrl: 'app/+utilisateur/components/utilisateur.component.html',
+    styleUrls: ['app/+utilisateur/components/utilisateur.component.css'],
+    directives: [FORM_DIRECTIVES, CORE_DIRECTIVES]
+})
+export class UtilisateurComponent {
+    id:String;
+
+    constructor(
+        private _router:Router,
+        private _routeParams:RouteParams){}
+    
+    ngOnInit() {
+        this.id = this._routeParams.get('id');
+    }
+
+}
