337f658a1834966e5f77661e58a2a0fce14a8269
hverlin <hugues.verlin@gmail.com>
2016-04-26 09:30:12 +0200

    ajout de la route pour la carte

---
+++ src/client/app/+carte/components/carte.component.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/core';
+import {CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/common';
+
+@Component({
+  selector: 'sd-home',
+  templateUrl: 'app/+carte/components/carte.component.html',
+  styleUrls: ['app/+carte/components/carte.component.css'],
+  directives: [FORM_DIRECTIVES, CORE_DIRECTIVES]
+})
+export class CarteComponent {}
--- src/client/app/components/app.component.ts
+++ src/client/app/components/app.component.ts
@@ -3,24 +3,32 @@ import {ROUTER_DIRECTIVES, RouteConfig} from 'angular2/router';
 import {NavbarComponent} from './navbar.component';
 import {NameListService} from '../shared/index';
 import {HomeComponent} from '../+home/index';
+import {CarteComponent} from '../+carte/index';
 import {AboutComponent} from '../+about/index';
 
 @Component({
-  selector: 'sd-app',
-  viewProviders: [NameListService],
-  templateUrl: 'app/components/app.component.html',
-  directives: [ROUTER_DIRECTIVES, NavbarComponent]
+    selector: 'sd-app',
+    viewProviders: [NameListService],
+    templateUrl: 'app/components/app.component.html',
+    directives: [ROUTER_DIRECTIVES, NavbarComponent]
 })
+
 @RouteConfig([
-  {
-    path: '/',
-    name: 'Home',
-    component: HomeComponent
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: AboutComponent
-  }
+    {
+        path: '/',
+        name: 'Home',
+        component: HomeComponent
+    },
+    {
+        path: '/carte',
+        name: 'Carte',
+        component: CarteComponent
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: AboutComponent
+    }
 ])
-export class AppComponent {}
+export class AppComponent {
+}
