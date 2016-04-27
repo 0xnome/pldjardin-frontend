6374a55886f13ae0456ca1904734df21a7a6258d
elmhaidara <elmhaidara@gmail.com>
2016-04-27 14:30:51 +0200

    Fix dernier truc avant de partir sur de bonnes bases

--- src/client/app/+carte/components/carte.component.ts
+++ src/client/app/+carte/components/carte.component.ts
@@ -1,5 +1,3 @@
-/// <reference path="leaflet.d.ts" />
-
 import {Component} from 'angular2/core';
 import {CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/common';
 
@@ -17,13 +15,13 @@ export class CarteComponent {
   ACCES_TOKEN = "pk.eyJ1IjoiZWxtaGFpZGFyYSIsImEiOiJjaW5pbXBjdjEwMDRidnNrbDJ4N2NoMG5iIn0.QF5aeo-TKr0Vw_J93gwx1A";
 
   ngOnInit() {
-    var mymap = L.map('mapid').setView([51.505, -0.09], 5);
+ /*   var mymap = L.map('mapid').setView([51.505, -0.09], 5);
 
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
       attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
       maxZoom: 18,
       id: 'smartgarden',
       accessToken: this.ACCES_TOKEN
-    }).addTo(mymap);
+    }).addTo(mymap); */
   }
 }
