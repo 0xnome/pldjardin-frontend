d4dff1e3dd337460b09a3124f2dc0a8b3773fabf
mohamed <med.tn.sfax@gmail.com>
2016-04-27 10:56:53 +0200

    ajout du composant et service pour les actualités (update)

--- src/client/app/shared/services/actualite.service.ts
+++ src/client/app/shared/services/actualite.service.ts
@@ -1,7 +1,7 @@
 import {Observable}     from 'rxjs/Observable';
 import {Injectable}     from 'angular2/core';
 import {Http, Response} from 'angular2/http';
-import {Actualite} from "intefaces";
+import {Actualite} from "interfaces";
 
 
 
--- src/client/app/shared/services/interfaces.ts
+++ src/client/app/shared/services/interfaces.ts
@@ -1,26 +1,25 @@
-
 export interface Adresse {
-    id: number,
-    ville:string,
-    code_postal: string,
-    rue: string,
-    long: string,
-    lat: string
+  id:number,
+  ville:string,
+  code_postal:string,
+  rue:string,
+  long:string,
+  lat:string
 }
 
 export interface Jardin {
-    id: number;
-    nom: string;
-    horaire: string;
-    image: string;
-    description: string;
-    restreint:boolean;
-    adresse:Adresse
+  id:number;
+  nom:string;
+  horaire:string;
+  image:string;
+  description:string;
+  restreint:boolean;
+  adresse:Adresse
 }
 
 export interface Actualite {
-  id: number;
-  jardin: number;
-  texte: string;
-  date_creation: string;
+  id:number;
+  jardin:number;
+  texte:string;
+  date_creation:string;
 }
