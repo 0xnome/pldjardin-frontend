e2a0fe4e8cdd26e88e1d374639eb5d1e0c8c0d25
elmhaidara <elmhaidara@gmail.com>
2016-04-28 11:55:16 +0200

    ajout jquery + fix hauteur de la map : reste un bug quand on change de page

--- src/client/app/+carte/components/carte.component.ts
+++ src/client/app/+carte/components/carte.component.ts
@@ -2,7 +2,8 @@ import {Component} from 'angular2/core';
 import {CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/common';
 import {CarteService} from "../../shared/index";
 import 'leaflet'
-
+import 'jquery'
+import Map = L.Map;
 
 @Component({
   selector: 'sd-home',
@@ -14,18 +15,28 @@ import 'leaflet'
 
 export class CarteComponent {
 
-  constructor(private carteService : CarteService){}
+  map:Map;
+
+  constructor(private carteService:CarteService) {
+  }
 
   ngOnInit() {
-    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-    this.carteService.baseMaps.MapBoxStreets.addTo(mymap);
+    this.map = L.map('mapid').setView([51.505, -0.09], 10);
+    this.carteService.baseMaps.OpenStreetMap.addTo(this.map);
+
+    $(window).on("resize", () => {
+      $("#mapid").height($(window).height() - 90).width($(window).width());
+      this.map.invalidateSize(false);
+    }).trigger("resize");
+
+    console.log('ngOnint');
+
   }
 
   getMapHeight() {
     // TODO à completer
-    return window.innerHeight-53;
+    return window.innerHeight - 75;
   }
 
 }
-
 
