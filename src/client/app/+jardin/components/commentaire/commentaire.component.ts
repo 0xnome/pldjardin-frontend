16551dc264659723f4970055bde41a74c272d951
hverlin <hugues.verlin@gmail.com>
2016-04-30 16:29:47 +0200

    ajout icone suppresion que si auteur du commentaire

--- src/client/app/+jardin/components/commentaire/commentaire.component.ts
+++ src/client/app/+jardin/components/commentaire/commentaire.component.ts
@@ -4,17 +4,19 @@ import {CommentaireJardinService} from "../../../shared/index";
 import {CommentaireJardin,Jardin} from "../../../shared/index";
 import {NomUtilisateurComponent} from "../nom-utilisateur/nom-utilsateur.component";
 import {AvatarUtilisateurComponent} from "../avatar-utilisateur/avatar-utilsateur.component";
+import {AuthService} from "../../../shared/services/auth.service";
 
 @Component({
   selector: 'sd-commentaire',
   templateUrl: 'app/+jardin/components/commentaire/commentaire.component.html',
   styleUrls: ['app/+jardin/components/commentaire/commentaire.component.css'],
   directives: [FORM_DIRECTIVES, CORE_DIRECTIVES,NomUtilisateurComponent,AvatarUtilisateurComponent],
-  providers: [CommentaireJardinService]
+  providers: [CommentaireJardinService, AuthService]
 })
 export class CommentaireComponent {
 
-  constructor(private commentaireJardinService:CommentaireJardinService) {
+  constructor(private commentaireJardinService:CommentaireJardinService,
+                private authService:AuthService) {
   }
 
   @Input() num:number;
@@ -34,6 +36,10 @@ export class CommentaireComponent {
   deleteCommentaire(id) {
     this.deleteCommentaireEvent.emit(id);
   }
+
+    peutSupprimer(){
+        return this.commentaireJardin.auteur == this.authService.getId()
+    }
 
   convertdate(date:string) {
     return new Date(date);
