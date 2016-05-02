6cb62c9f092d31509a9e349275afb8de0f25f0d4
hverlin <hugues.verlin@gmail.com>
2016-05-02 08:30:44 +0200

    ajout get api conditionnel

--- src/client/app/shared/config.ts
+++ src/client/app/shared/config.ts
@@ -1,4 +1,12 @@
 
 export class Config{
+    
+    static getApiUrl(url:string){
+        if(url.indexOf(Config.api) ===  -1){ //
+            return Config.api + url
+        }
+        return url
+    }
+
     static api = "http://localhost:8000"
 }
